c0da5e9033fcfc008b2ad16e2b57f344661a779c
Maria Yeromina <mariayeromina@Marias-MacBook-Air.local>
2023-12-25 21:05:34 +0100

    hw 5 movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
+import { Home } from '../pages/Home';
+import { MovieSearch } from '../pages/MovieSearch';
+import { NavLink, Navigate, Route, Routes } from 'react-router-dom';
+import { MovieDetails } from '../pages/MovieDetails';
+import { Cast } from './Cast';
+import { Review } from './Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <nav className={'navigation'}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<MovieSearch />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Review />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 };
---
+++ src/pages/MovieSearch.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { getMovieByQuery } from '../routes/api';
+import { Link, useSearchParams } from 'react-router-dom';
+
+export const MovieSearch = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const value = event.currentTarget.elements.search.value.trim();
+
+    if (!value) {
+      return;
+    }
+
+    setSearchParams({ query: value });
+  };
+
+  useEffect(() => {
+    const query = searchParams.get('query');
+
+    if (query) {
+      setLoading(true);
+
+      getMovieByQuery(query)
+        .then(res => {
+          setMovies(res);
+          setLoading(false);
+        })
+        .catch(err => {
+          console.log(err.message);
+          setLoading(false);
+        });
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="form">
+        <input type="text" placeholder="Search movie" name="search" />
+        <button type="submit">Search</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      <div>
+        <ul className="movieList">
+          {movies.map(({ title, id }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`}>{title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
